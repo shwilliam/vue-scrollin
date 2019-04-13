ac23a5aad09ed91c198e67117ebdfa901afa243d
William L <w-lindvall@outlook.com>
2019-04-13 00:42:16 -0700

    refactor: cleanup

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,29 +1,27 @@
-// rollup.config.js
-import vue from 'rollup-plugin-vue';
-import buble from 'rollup-plugin-buble';
-import uglify from 'rollup-plugin-uglify-es';
-import minimist from 'minimist';
+import vue from 'rollup-plugin-vue'
+import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify-es'
+import minimist from 'minimist'
 
-const argv = minimist(process.argv.slice(2));
+const argv = minimist(process.argv.slice(2))
 
 const config = {
-    input: 'src/index.js',
-    output: {
-        name: 'VScrollin',
-        exports: 'named',
-    },
-    plugins: [
-        vue({
-            css: true,
-            compileTemplate: true,
-        }),
-        buble(),
-    ],
-};
-
-// Only minify browser (iife) version
-if (argv.format === 'iife') {
-    config.plugins.push(uglify());
+  input: 'src/index.js',
+  output: {
+    name: 'VScrollin',
+    exports: 'named'
+  },
+  plugins: [
+    vue({
+      css: true,
+      compileTemplate: true
+    }),
+    buble()
+  ]
 }
 
-export default config;
+if (argv.format === 'iife') {
+  config.plugins.push(uglify())
+}
+
+export default config
--- src/v-scrollin.vue
+++ src/v-scrollin.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="v-scrollin">
-    {{scrollingText}}
+    {{ scrollingText }}
   </div>
 </template>
 
 <script>
-import { setInterval, clearInterval } from "timers";
+import { setInterval, clearInterval } from 'timers'
 
 export default {
-  name: "VScrollin",
+  name: 'VScrollin',
   props: {
     characters: {
       // characters used during flicker
       type: Array,
       required: false,
-      default: () => "qwertyuiopasdfghjklzxcvbnm".split("")
+      default: () => 'qwertyuiopasdfghjklzxcvbnm'.split('')
     },
     misses: {
       // amount of scrolls before correct letter
@@ -29,42 +29,40 @@ export default {
       default: 80
     }
   },
-  data() {
+  data () {
     return {
       amountFinished: 0,
       currentMisses: 0,
       finishedText: this.$slots.default[0].text,
       scroll: null,
-      scrollingText: "",
+      scrollingText: '',
       workingIndices: []
-    };
+    }
   },
-  mounted() {
-    this.fillRandomLetters();
+  mounted () {
+    this.fillRandomLetters()
     this.scroll = setInterval(() => {
-      this.tick();
-    }, this.speed);
+      this.tick()
+    }, this.speed)
   },
   methods: {
-    fillRandomLetters() {
-      const startingLetters = [];
+    fillRandomLetters () {
+      const startingLetters = []
       for (let n = 0; n < this.finishedText.length; n++) {
-        startingLetters.push(this.getRandomLetter());
+        startingLetters.push(this.getRandomLetter())
       }
-      this.scrollingText = startingLetters.join("");
+      this.scrollingText = startingLetters.join('')
     },
-    getRandomLetter() {
-      return this.characters[
-        Math.floor(Math.random() * this.characters.length)
-      ];
+    getRandomLetter () {
+      return this.characters[Math.floor(Math.random() * this.characters.length)]
     },
-    replace(str, i, replacement) {
+    replace (str, i, replacement) {
       if (!replacement) return str
       return (
         str.substr(0, i) + replacement + str.substr(i + replacement.length)
-      );
+      )
     },
-    tick() {
+    tick () {
       for (
         let n = this.amountFinished;
         n < this.amountFinished + this.workingIndices.length;
@@ -74,37 +72,37 @@ export default {
           this.scrollingText,
           n,
           this.getRandomLetter()
-        );
+        )
       }
       if (this.workingIndices.length < 3 && !this.amountFinished) {
-        this.workingIndices.push(this.workingIndices.length);
+        this.workingIndices.push(this.workingIndices.length)
       }
       if (
         this.scrollingText === this.finishedText &&
         this.currentMisses === this.misses
       ) {
-        clearInterval(this.scroll);
+        clearInterval(this.scroll)
       } else if (++this.currentMisses === this.misses) {
         this.scrollingText = this.replace(
           this.scrollingText,
           this.amountFinished,
           this.finishedText[this.amountFinished]
-        );
-        this.currentMisses = 0;
+        )
+        this.currentMisses = 0
         if (++this.amountFinished >= this.finishedText.length - 2) {
           this.workingIndices
-            .sort(function(a, b) {
-              return a - b;
+            .sort(function (a, b) {
+              return a - b
             })
-            .shift();
+            .shift()
         } else {
-          this.workingIndices.sort(function(a, b) {
-            return a - b;
+          this.workingIndices.sort(function (a, b) {
+            return a - b
           })[0] =
-            this.amountFinished + 3;
+            this.amountFinished + 3
         }
       }
     }
   }
-};
+}
 </script>
