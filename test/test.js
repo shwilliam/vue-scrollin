e8a3c004ad6ad5a73207830277f9e2163a344ae2
William L <w-lindvall@outlook.com>
2019-04-14 22:33:43 -0700

    tests: add getRandomLetter from array

--- test/test.js
+++ test/test.js
@@ -8,10 +8,22 @@ describe('getRandomLetter', function () {
     assert.strictEqual(typeof getRandomLetter('ert123%^&'), 'string')
   })
 
-  it('should return a value from param', function () {
+  it('should return a value from string', function () {
     assert.strictEqual('asd'.includes(getRandomLetter('asd')), true)
     assert.strictEqual('234'.includes(getRandomLetter('234')), true)
     assert.strictEqual('ert123%^&'.includes(getRandomLetter('ert123%^&')), true)
+  })
+
+  it('should return a value from array', function () {
+    assert.strictEqual(['a', 's', 'd'].includes(getRandomLetter(['a', 's', 'd'])), true)
+    assert.strictEqual([2, 3, 4].includes(getRandomLetter([2, 3, 4])), true)
+    assert.strictEqual(
+      ['e', 'r', 't', 1, 2, 3, '%', '^', '&']
+        .includes(
+          getRandomLetter(['e', 'r', 't', 1, 2, 3, '%', '^', '&'])
+        )
+      , true
+    )
   })
 })
 
